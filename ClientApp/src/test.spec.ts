1152b4939df5d58a22456fff87eeb3b754a818f1
donaral <duyguonaral@gmail.com>
2019-02-12 22:22:57 +0300

    angularjs-testing-article sample tests to fail some unit tests

--- ClientApp/src/test.spec.ts
+++ ClientApp/src/test.spec.ts
@@ -1,10 +1,41 @@
-using System;
-namespace LinnworksTest.ClientApp.src
-{
-  public class test
-  {
-    public test()
-    {
-    }
-  }
-}
+import { AppModule } from './app/app.module';
+
+
+
+describe("Midway: Testing Modules", () => {
+
+  describe("App Module:", () => {
+    
+    it("should be registered", function() {
+      expect(module).not.toBeNull;
+    });
+
+    describe("Dependencies:", function() {
+      let deps;
+      let hasModule = (m) => {
+        return deps.index(m) >= 0;
+      };
+      
+      
+      it("should have App.Controllers as a dependency", () => {
+        expect(hasModule('App.Controllers')).toBe(true);
+      });
+      
+      it("should have App.Directives as a dependency", () => {
+        expect(hasModule('App.Directives')).toBe(true);
+      });
+      
+      it("should have App.Filters as a dependency", () => {
+        expect(hasModule('App.Filters')).toBe(true);
+      });
+      
+      it("should have App.Routes as a dependency", () => {
+        expect(hasModule('App.Routes')).toBe(true);
+      });
+      
+      it("should have App.Services as a dependency", () => {
+        expect(hasModule('App.Services')).toBe(true);
+      });
+    });
+  });
+});
