c9782747d39e5dda34a9eda5a09c4498a1d58cca
donaral <duyguonaral@gmail.com>
2019-02-26 22:45:43 +0300

    initial unit tests

--- ClientApp/src/test.spec.ts
+++ ClientApp/src/test.spec.ts
@@ -1,41 +1,55 @@
-import { AppModule } from './app/app.module';
+import { Component } from '@angular/core';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule, Routes } from '../node_modules/@angular/router';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
 
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component'; 
+import { LoginComponent } from './app/login/login.component';
+import { NavMenuComponent } from './app/nav-menu/nav-menu.component';
 
+describe( 'AppComponent', () => {
+  const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+//    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full'}
+  
+  ];
+  
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        HomeComponent,
+        NavMenuComponent
+      ],
+      imports: [
+        RouterModule.forRoot(routes)
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+    }).compileComponents();
+  }));    
 
-describe("Midway: Testing Modules", () => {
-
-  describe("App Module:", () => {
+  it('should have correct title', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Duygu');
+  }));  
     
-    it("should be registered", function() {
-      expect(module).not.toBeNull;
-    });
-
-    describe("Dependencies:", function() {
-      let deps;
-      let hasModule = (m) => {
-        return deps.index(m) >= 0;
-      };
-      
-      
-      it("should have App.Controllers as a dependency", () => {
-        expect(hasModule('App.Controllers')).toBe(true);
-      });
-      
-      it("should have App.Directives as a dependency", () => {
-        expect(hasModule('App.Directives')).toBe(true);
-      });
-      
-      it("should have App.Filters as a dependency", () => {
-        expect(hasModule('App.Filters')).toBe(true);
-      });
-      
-      it("should have App.Routes as a dependency", () => {
-        expect(hasModule('App.Routes')).toBe(true);
-      });
-      
-      it("should have App.Services as a dependency", () => {
-        expect(hasModule('App.Services')).toBe(true);
-      });
-    });
-  });
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ...');
+  }));  
 });
+
+// describe('LoginComponent')
+
+// LoginComponent,
+// NavMenuComponent
---
+++ ClientApp/src/test2.spec.ts
@@ -0,0 +1,10 @@
+using System;
+namespace LinnworksTest.ClientApp.src
+{
+  public class test2
+  {
+    public test2()
+    {
+    }
+  }
+}
